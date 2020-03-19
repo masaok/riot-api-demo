aeb3c22e3c535c5dad973cfcc46d8291de4c741d
Masao Kitamura <masaok@gmail.com>
2020-03-19 04:24:15 -0700

    Added Expect calls

--- tests/status.test.js
+++ tests/status.test.js
@@ -4,15 +4,19 @@ const { API_KEY } = process.env
 
 const url = `https://na1.api.riotgames.com/lol/status/v3/shard-data?api_key=${API_KEY}`
 
+// https://developer.riotgames.com/apis#lol-status-v3/GET_getShardData
 describe('Status', () => {
-  var a
   it('fetches properly', async () => {
     const response = await fetch(url)
     const headers = response.headers
-    const data = response.json()
-    console.log('FETCH STATUS AGAIN HEADERS:')
-    console.log(headers)
+    const data = await response.json()
+    // console.log('FETCH STATUS AGAIN HEADERS:')
+    // console.log(headers)
     console.log('FETCH STATUS AGAIN DATA:')
     console.log(data)
+
+    // https://jestjs.io/docs/en/expect
+    expect('name' in data).toBeTruthy()
+    expect(typeof data.name).toBe('string')
   })
 })
