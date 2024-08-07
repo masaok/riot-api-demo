283e1f6d73613ab57d2b15b510b7b04a7cd79abc
Masao Kitamura <masaok@gmail.com>
2024-08-07 01:38:36 -0700

    Trying to get Riot API to work again

--- examples/summonerTest.js
+++ examples/summonerTest.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+// import dotenv from 'dotenv'
+const dotenv = require('dotenv')
+dotenv.config()
+
 const fetch = require('node-fetch')
 
 // console.log(process.env)
@@ -10,10 +14,19 @@ const { API_KEY, SUMMONER_NAME } = process.env
 
 // const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${SUMMONER_NAME}?api_key=${API_KEY}`
 const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/me?api_key=${API_KEY}`
+console.log(url)
 
 // Function as variable
 const fetchAPI = async () => {
-  let response = await fetch(url)
+  // let response = await fetch(url)
+
+  // Fetch with X-Riot-Token header
+  let response = await fetch(url, {
+    headers: {
+      'X-Riot-Token': API_KEY,
+    },
+  })
+
   return response
 }
 
